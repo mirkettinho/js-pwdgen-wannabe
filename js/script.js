2bd81f0ebe5c0fbddea7a5ab5b489ed4b806b616
mirkettinho <mirkodifranco30@gmail.com>
2023-03-17 15:35:23 +0100

    first commit

---
+++ js/script.js
@@ -0,0 +1,9 @@
+// chiedere all'utente il nome, cognome, colore preferito e unire le informazioni ricevute  
+
+
+/*
+1. chiedere e salvare il nome.
+2. chiedere e salvare il cognome.
+3. chiedere e salvare il colore preferito.
+4. unire le informazioni
+*/
